199e0d34167bdc37ae1491e662dae710715f2bee
Yash <maddysai9848@gmail.com>
2025-11-28 18:02:29 +0530

    Updates

---
+++ _app/immutable/chunks/krHCONzu.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1jsepp8?.base??"",e=globalThis.__sveltekit_1jsepp8?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/n_sGk69h.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_eciwjh?.base??"",e=globalThis.__sveltekit_eciwjh?.assets??s??"";export{e as a,s as b};
